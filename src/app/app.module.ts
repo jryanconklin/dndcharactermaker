c4fafad1c2043f41098d3ee9f378836fd8a74312
J. Ryan Conklin <jryanconklin@gmail.com>
2016-12-07 09:33:16 -0800

    Add Firebase to Project and Confirm Connection with Seed Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  // Add apiKey to .Env File and Obscure?
+  apiKey: "AIzaSyCOB2eepNH4Xp2TCqpKKWrr7G1UqcuJATs",
+   authDomain: "dndcharactercreator-1b7d3.firebaseapp.com",
+   databaseURL: "https://dndcharactercreator-1b7d3.firebaseio.com",
+   storageBucket: "dndcharactercreator-1b7d3.appspot.com",
+   messagingSenderId: "623544075227"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
